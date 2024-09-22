5df17f4ae10b348dae8beb473b808747910763d1
Yunchov98 <georgiiunchov@gmail.com>
2024-09-22 23:01:47 +0300

    Create custom pipe for dates

--- src/app/shared/pipes/custom-date.pipe.ts
+++ src/app/shared/pipes/custom-date.pipe.ts
@@ -1,8 +1,19 @@
 import { Pipe } from '@angular/core';
 
 @Pipe({
-    name: 'customDate'
+    name: 'customDate',
 })
 export class CustomDatePipe {
-    // Add your code here
+    transform(value: Date): string {
+        if (!value) {
+            return '';
+        }
+
+        const date = new Date(value);
+        const day = ('0' + date.getDate()).slice(-2);
+        const month = ('0' + (date.getMonth() + 1)).slice(-2);
+        const year = date.getFullYear();
+
+        return `${day}.${month}.${year}`;
+    }
 }
