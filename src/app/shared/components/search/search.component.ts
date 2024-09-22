edac648a9d2c753e99b3ebf43fd530ad8453f276
Yunchov98 <georgiiunchov@gmail.com>
2024-09-22 23:00:50 +0300

    Add input and output properties

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent {
-  // Use the name `placeholder` for the @Input.
-  // Use the name `search` for the @Output.
-}
+    @Input() placeholder!: string;
+    @Output() search = new EventEmitter<string>();
 
+    searchQuery!: string;
+
+    onSearch() {
+        if (this.searchQuery.trim()) {
+            this.search.emit(this.searchQuery.trim());
+        }
+    }
+}
