cd191394a14b8737e32573bbc576bc61167f6673
Yunchov98 <georgiiunchov@gmail.com>
2024-09-22 22:59:10 +0300

    Add login functionality

--- src/app/shared/components/login-form/login-form.component.ts
+++ src/app/shared/components/login-form/login-form.component.ts
@@ -1,12 +1,20 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { faIcons } from '@app/shared/common/fa-icons';
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss'],
+    selector: 'app-login-form',
+    templateUrl: './login-form.component.html',
+    styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
-  @ViewChild("loginForm") public loginForm!: NgForm;
-  //Use the names `email` and `password` for form controls.
+    @ViewChild('loginForm') public loginForm!: NgForm;
+    isFormSubmmited!: boolean;
+
+    eyeIcon = faIcons.eye;
+    eyeSlashIcon = faIcons.eyeSlash;
+
+    onSubmit(): void {
+        this.isFormSubmmited = true;
+    }
 }
