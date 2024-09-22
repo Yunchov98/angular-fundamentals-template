075fef767f855b776d613325351976af1c958375
Yunchov98 <georgiiunchov@gmail.com>
2024-09-22 23:01:13 +0300

    Create email validator directive

--- src/app/shared/directives/email.directive.ts
+++ src/app/shared/directives/email.directive.ts
@@ -1,9 +1,33 @@
-import { Directive } from "@angular/core";
+import { Directive } from '@angular/core';
+import {
+    AbstractControl,
+    NG_VALIDATORS,
+    ValidationErrors,
+    Validator,
+} from '@angular/forms';
+
+const emailRegEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
 @Directive({
     selector: '[emailValidator]',
-    providers: [/*Add your code here*/]
+    providers: [
+        {
+            provide: NG_VALIDATORS,
+            useExisting: EmailValidatorDirective,
+            multi: true,
+        },
+    ],
 })
-export class EmailValidatorDirective {
-    // Add your code here
+export class EmailValidatorDirective implements Validator {
+    validate(control: AbstractControl): ValidationErrors | null {
+        const email = control.value;
+
+        if (!email) {
+            return null;
+        }
+
+        const isEmailValid = emailRegEx.test(email);
+
+        return isEmailValid ? null : { invalidEmail: true };
+    }
 }
