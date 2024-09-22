45ed0668466e431cef3574501f170a36fdf7859d
Yunchov98 <georgiiunchov@gmail.com>
2024-09-22 22:59:46 +0300

    Add register functionality

--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { faIcons } from '@app/shared/common/fa-icons';
 
 @Component({
-  selector: 'app-registration-form',
-  templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.scss'],
+    selector: 'app-registration-form',
+    templateUrl: './registration-form.component.html',
+    styleUrls: ['./registration-form.component.scss'],
 })
-export class RegistrationFormComponent {
-  registrationForm!: FormGroup;
-  // Use the names `name`, `email`, `password` for the form controls.
+export class RegistrationFormComponent implements OnInit {
+    registrationForm!: FormGroup;
+    isFormSubmmited!: boolean;
+    isValid!: boolean;
+
+    eyeIcon = faIcons.eye;
+    eyeSlashIcon = faIcons.eyeSlash;
+
+    ngOnInit(): void {
+        this.registrationForm = new FormGroup({
+            name: new FormControl(null, [
+                Validators.required,
+                Validators.minLength(6),
+            ]),
+            email: new FormControl(null, [Validators.required]),
+            password: new FormControl(null, [Validators.required]),
+        });
+    }
+
+    onRegister(): void {
+        this.isFormSubmmited = true;
+        console.log(this.registrationForm);
+    }
 }
