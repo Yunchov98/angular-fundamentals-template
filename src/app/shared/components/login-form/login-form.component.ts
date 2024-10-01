a8a41d6c8209d40e604281964b05a7a5c8e804d4
Yunchov98 <georgiiunchov@gmail.com>
2024-10-01 21:53:17 +0300

    Add login functionality

--- src/app/shared/components/login-form/login-form.component.ts
+++ src/app/shared/components/login-form/login-form.component.ts
@@ -1,20 +1,54 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '@app/auth/services/auth.service';
+import { ROUTES } from '@app/core/environments/endpoints';
+import User from '@app/core/interfaces/user.interface';
 import { faIcons } from '@app/shared/common/fa-icons';
+import { UserStoreService } from '@app/user/services/user-store.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-login-form',
     templateUrl: './login-form.component.html',
     styleUrls: ['./login-form.component.scss'],
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnDestroy {
     @ViewChild('loginForm') public loginForm!: NgForm;
     isFormSubmmited!: boolean;
+    loginSubscribe$!: Subscription;
+    errorMessage!: string;
 
     eyeIcon = faIcons.eye;
     eyeSlashIcon = faIcons.eyeSlash;
 
+    constructor(
+        private authService: AuthService,
+        private userStoreService: UserStoreService,
+        private router: Router
+    ) {}
+
     onSubmit(): void {
-        this.isFormSubmmited = true;
+        const user: User = {
+            name: '',
+            email: this.loginForm.value.email,
+            password: this.loginForm.value.password,
+        };
+
+        this.loginSubscribe$ = this.authService.login(user).subscribe({
+            next: () => {
+                this.userStoreService.getUser();
+                this.router.navigate([ROUTES.courses]);
+            },
+            error: (error) => {
+                this.errorMessage = error.error.result;
+            },
+        });
+    }
+
+    ngOnDestroy(): void {
+        if (this.loginSubscribe$ !== undefined) {
+            this.loginSubscribe$.unsubscribe();
+        }
     }
 }
