a10b40ab466cfd8da956113d238bedbb9b71050f
Yunchov98 <georgiiunchov@gmail.com>
2024-10-01 21:46:46 +0300

    Create not auth guard

--- src/app/auth/guards/not-authorized.guard.ts
+++ src/app/auth/guards/not-authorized.guard.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Router, UrlTree } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
+import { ROUTES } from '@app/core/environments/endpoints';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class NotAuthorizedGuard {
-    // Add your code here
+    constructor(private authService: AuthService, private router: Router) {}
+
+    canActive(): boolean | UrlTree {
+        if (this.authService.isAuthorized) {
+            return this.router.createUrlTree([ROUTES.courses]);
+        }
+
+        return true;
+    }
 }
