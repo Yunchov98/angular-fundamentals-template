fb6d97ef48e38d14a50e83a2d88ed43922beccea
Yunchov98 <georgiiunchov@gmail.com>
2024-10-01 21:46:29 +0300

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
@@ -6,23 +7,41 @@ import { SharedModule } from '@shared/shared.module';
 import { CoursesModule } from './features/courses/courses.module';
 
 import { AppComponent } from '@app/app.component';
-import { CourseInfoComponent } from '@features/course-info/course-info.component';
 import { CoursesComponent } from './features/courses/courses.component';
+import { CourseInfoComponent } from '@features/course-info/course-info.component';
 
-import { NotAuthorizedGuard } from '@app/auth/guards/not-authorized.guard';
 import { AuthorizedGuard } from '@app/auth/guards/authorized.guard';
+import { NotAuthorizedGuard } from '@app/auth/guards/not-authorized.guard';
 
-import { CoursesStoreService } from '@app/services/courses-store.service';
+import { routes } from './app-routing.module';
 import { CoursesService } from '@app/services/courses.service';
+import { CoursesStoreService } from '@app/services/courses-store.service';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { UserModule } from './user/user.module';
+import { TokenInterceptor } from './auth/interceptors/token.interceptor';
 
 @NgModule({
     declarations: [AppComponent, CourseInfoComponent, CoursesComponent],
-    imports: [BrowserModule, SharedModule, FontAwesomeModule, CoursesModule],
+    imports: [
+        BrowserModule,
+        SharedModule,
+        FontAwesomeModule,
+        CoursesModule,
+        HttpClientModule,
+        RouterModule.forRoot(routes),
+        UserModule,
+    ],
     providers: [
         AuthorizedGuard,
         NotAuthorizedGuard,
         CoursesService,
         CoursesStoreService,
+        { provide: Window, useValue: window },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: TokenInterceptor,
+            multi: true,
+        },
     ],
     bootstrap: [AppComponent],
 })
