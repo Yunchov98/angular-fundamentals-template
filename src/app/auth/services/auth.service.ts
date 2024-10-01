c99b7c4b9586786f0fff583d96d490d924966cc0
Yunchov98 <georgiiunchov@gmail.com>
2024-10-01 21:47:15 +0300

    Create auth service and session storage service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,30 +1,65 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+
+import User from '@app/core/interfaces/user.interface';
+
+import { SessionStorageService } from './session-storage.service';
+
+import { CONSTANTS } from '@app/core/environments/constants';
+import { ENDPOINTS } from '@app/core/environments/endpoints';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class AuthService {
-    login(user: any) { // replace 'any' with the required interface
-        // Add your code here
+    private isAuthorized$$ = new BehaviorSubject<boolean>(false);
+    public isAuthorized$: Observable<boolean> =
+        this.isAuthorized$$.asObservable();
+    private apiUrl = CONSTANTS.host;
+
+    constructor(
+        private http: HttpClient,
+        private sessionStorage: SessionStorageService
+    ) {}
+
+    login(user: User): Observable<{
+        successful: boolean;
+        result: string;
+        user: User;
+    }> {
+        return this.http
+            .post<{
+                successful: boolean;
+                result: string;
+                user: User;
+            }>(this.apiUrl + ENDPOINTS.login, user)
+            .pipe(
+                tap((response) => {
+                    this.sessionStorage.setToken(response.result);
+                    this.isAuthorized$$.next(true);
+                })
+            );
     }
 
-    logout() {
-        // Add your code here
+    logout(): void {
+        this.sessionStorage.deleteToken();
+        this.isAuthorized$$.next(false);
     }
 
-    register(user: any) { // replace 'any' with the required interface
-        // Add your code here
+    register(user: User): Observable<User> {
+        return this.http.post<User>(this.apiUrl + ENDPOINTS.register, user);
     }
 
-    get isAuthorised() {
-        // Add your code here. Get isAuthorized$$ value
+    get isAuthorized(): boolean {
+        return this.sessionStorage.getToken() !== null;
     }
 
-    set isAuthorised(value: boolean) {
-        // Add your code here. Change isAuthorized$$ value
+    set isAuthorized(value: boolean) {
+        this.isAuthorized$$.next(value);
     }
 
-    getLoginUrl() {
-        // Add your code here
+    getLoginUrl(): string {
+        return ENDPOINTS.login;
     }
 }
