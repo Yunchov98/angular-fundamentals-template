c8a03c786f57e9d2ae7f3c5a05306892ecc75e58
Yunchov98 <georgiiunchov@gmail.com>
2024-10-01 23:44:28 +0300

    return observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import Course from './core/interfaces/course';
+
 import { UserStoreService } from './user/services/user-store.service';
 import { AuthService } from './auth/services/auth.service';
+
+import Course from './core/interfaces/course';
 import UserMe from './core/interfaces/user-me';
-import { UserService } from './user/services/user.service';
 
 @Component({
     selector: 'app-root',
@@ -14,17 +15,15 @@ export class AppComponent implements OnInit {
     title = 'courses-app';
     courses!: { successful: boolean; result: Course[] };
     errorMessage!: string;
-    user?: UserMe;
+    user?: UserMe | null;
 
     constructor(
         private userStoreService: UserStoreService,
-        private authService: AuthService,
-        private userService: UserService,
+        private authService: AuthService
     ) {}
 
     ngOnInit(): void {
-        // this.userStoreService.getUser();
-        this.userService.getUser().subscribe({
+        this.userStoreService.getUser().subscribe({
             next: (user) => {
                 this.user = user;
             },
