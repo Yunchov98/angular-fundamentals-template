ac6fdc333fb8857e6d690e20c05bad34ea12050d
Yunchov98 <georgiiunchov@gmail.com>
2024-10-01 21:54:41 +0300

    Create user store service and user service

--- src/app/user/services/user-store.service.ts
+++ src/app/user/services/user-store.service.ts
@@ -1,19 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { UserService } from './user.service';
+import UserMe from '@app/core/interfaces/user-me.interface';
+import { CONSTANTS } from '@app/core/environments/constants';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
-export class UserStoreService {
+export class UserStoreService implements OnDestroy {
+    private name$$ = new BehaviorSubject<string>('');
+    private isAdmin$$ = new BehaviorSubject<boolean>(false);
 
-    getUser() {
-        // Add your code here
+    public name$: Observable<string> = this.name$$.asObservable();
+    public isAdmin$: Observable<boolean> = this.isAdmin$$.asObservable();
+
+    subscription$!: Subscription;
+
+    constructor(private userService: UserService) {}
+
+    getUser(): void {
+        this.subscription$ = this.userService.getUser().subscribe({
+            next: (user: UserMe) => {
+                this.name$$.next(user.result.name);
+                this.isAdmin$$.next(user.result.role === CONSTANTS.adminRole);
+            },
+            error: () => {
+                this.name$$.next('');
+                this.isAdmin$$.next(false);
+            },
+        });
     }
 
-    get isAdmin() {
-        // Add your code here. Get isAdmin$$ value
+    get isAdmin(): boolean {
+        return this.isAdmin$$.getValue();
     }
 
     set isAdmin(value: boolean) {
-        // Add your code here. Change isAdmin$$ value
+        this.isAdmin$$.next(value);
+    }
+
+    ngOnDestroy(): void {
+        if (this.subscription$) {
+            this.subscription$.unsubscribe();
+        }
     }
 }
--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,10 +1,47 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionStorageService } from '@app/auth/services/session-storage.service';
+import { CONSTANTS } from '@app/core/environments/constants';
+import { ENDPOINTS, ROUTES } from '@app/core/environments/endpoints';
+import UserMe from '@app/core/interfaces/user-me.interface';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class UserService {
-    getUser() {
-        // Add your code here
+    private apiUrl = CONSTANTS.host;
+
+    constructor(
+        private sessionStorage: SessionStorageService,
+        private http: HttpClient,
+        private router: Router
+    ) {}
+
+    getUser(): Observable<UserMe> {
+        const TOKEN = this.sessionStorage.getToken();
+
+        if (TOKEN) {
+            const headers = new HttpHeaders({
+                Authorization: `${TOKEN}`,
+            });
+
+            return this.http
+                .get<UserMe>(this.apiUrl + ENDPOINTS.getUser, { headers })
+                .pipe(
+                    catchError((error) => {
+                        if (error.status === 401) {
+                            this.sessionStorage.deleteToken();
+                            this.router.navigate([ROUTES.login]);
+                        }
+                        return throwError(
+                            () => new Error('Failed to fetch user')
+                        );
+                    })
+                );
+        } else {
+            return throwError(() => new Error('Token is missing'));
+        }
     }
 }
