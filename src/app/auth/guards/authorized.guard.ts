fe339197157abccdeddcc7e72add396e24a90b16
Yunchov98 <georgiiunchov@gmail.com>
2024-10-01 21:46:39 +0300

    Create auth guard

--- src/app/auth/guards/authorized.guard.ts
+++ src/app/auth/guards/authorized.guard.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Router, UrlTree } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
+import { ROUTES } from '@app/core/environments/endpoints';
+import { UserStoreService } from '@app/user/services/user-store.service';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class AuthorizedGuard {
-    // Add your code here
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private userStoreService: UserStoreService
+    ) {}
+
+    canLoad(): boolean | UrlTree {
+        if (!this.authService.isAuthorized) {
+            return this.router.createUrlTree([ROUTES.login]);
+        }
+
+        const isAdmin = this.userStoreService.isAdmin;
+
+        if (isAdmin) {
+            return true;
+        }
+
+        return true;
+    }
 }
