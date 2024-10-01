85198f2ff9a3306d30bb5d0fd4ed833c402c2a59
Yunchov98 <georgiiunchov@gmail.com>
2024-10-01 21:50:17 +0300

    Add routes

--- src/app/features/courses/courses.module.ts
+++ src/app/features/courses/courses.module.ts
@@ -4,12 +4,31 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '@app/shared/shared.module';
 
 import { CourseListComponent } from './course-list/course-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import {
+    CourseCardComponent,
+    CourseFormComponent,
+} from '@app/shared/components';
+import { CoursesComponent } from './courses.component';
+
+import { AdminGuard } from '@app/user/guards/admin.guard';
 
 const components = [CourseListComponent];
 
+const routes: Routes = [
+    { path: 'courses', component: CoursesComponent },
+    { path: 'add', canActivate: [AdminGuard], component: CourseFormComponent },
+    {
+        path: 'edit/:id',
+        canActivate: [AdminGuard],
+        component: CourseFormComponent,
+    },
+    { path: ':id', component: CourseCardComponent },
+];
+
 @NgModule({
     declarations: [components],
-    imports: [CommonModule, SharedModule],
-    exports: [components],
+    imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
+    exports: [components, RouterModule],
 })
 export class CoursesModule {}
