fa77c87a0857f99f8002107bac6c124972b99274
Yunchov98 <georgiiunchov@gmail.com>
2024-10-01 21:52:00 +0300

    Update authors on init

--- src/app/shared/components/course-card/course-card.component.ts
+++ src/app/shared/components/course-card/course-card.component.ts
@@ -1,11 +1,13 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import Author from '@app/core/interfaces/author.interface';
+import { CoursesService } from '@app/services/courses.service';
 
 @Component({
     selector: 'app-course-card',
     templateUrl: './course-card.component.html',
     styleUrls: ['./course-card.component.scss'],
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements OnInit {
     @Input() title!: string;
     @Input() description!: string;
     @Input() creationDate!: Date;
@@ -14,6 +16,29 @@ export class CourseCardComponent {
     @Input() editable!: boolean;
 
     @Output() clickOnShow = new EventEmitter<void>();
+
+    populatedAuthors: string[] = [];
+    errorMessage!: string;
+
+    constructor(private coursesService: CoursesService) {}
+
+    ngOnInit(): void {
+        if (this.authors) {
+            this.authors.map((authorId) => {
+                this.coursesService.getAuthorById(authorId).subscribe({
+                    next: (response) => {
+                        if (response.successful) {
+                            this.populatedAuthors = [
+                                ...this.populatedAuthors,
+                                response.result.name,
+                            ];
+                        }
+                    },
+                    error: (error) => (this.errorMessage = error.error.message),
+                });
+            });
+        }
+    }
 
     onShowCourse() {
         this.clickOnShow.emit();
