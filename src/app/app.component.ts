4172f4e1deb9a824541a644310db03975ba6b44e
Yunchov98 <georgiiunchov@gmail.com>
2024-10-01 21:46:17 +0300

    Get user data on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,42 @@
-import { Component } from '@angular/core';
-import { mockedCoursesList } from './shared/mocks/mock';
+import { Component, OnInit } from '@angular/core';
+import Course from './core/interfaces/course.interface';
+import { UserStoreService } from './user/services/user-store.service';
+import { AuthService } from './auth/services/auth.service';
+import UserMe from './core/interfaces/user-me.interface';
+import { UserService } from './user/services/user.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'courses-app';
-    isLoggedIn: boolean = true;
+    courses!: { successful: boolean; result: Course[] };
+    errorMessage!: string;
+    user?: UserMe;
 
-    onLoginToggleHandler() {
-        this.isLoggedIn = !this.isLoggedIn;
+    constructor(
+        private userStoreService: UserStoreService,
+        private authService: AuthService,
+        private userService: UserService
+    ) {}
+
+    ngOnInit(): void {
+        this.userStoreService.getUser();
+        this.userService.getUser().subscribe({
+            next: (user) => {
+                this.user = user;
+            },
+            error: (error) => (this.errorMessage = error),
+        });
     }
 
-    onCourseSearch(query: string) {
-        console.log(query);
+    get isLoggedIn(): boolean {
+        return this.authService.isAuthorized;
+    }
+
+    onLogout() {
+        this.authService.logout();
     }
 }
