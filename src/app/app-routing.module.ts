74ff7820c48b6320d4435b8c1f744291d8f68561
Yunchov98 <georgiiunchov@gmail.com>
2024-10-01 21:45:49 +0300

    Add routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,26 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    /* Add your code here */
+    { path: '', redirectTo: '/courses', pathMatch: 'full' },
+    {
+        path: 'login',
+        loadChildren: () =>
+            import('./shared/shared.module').then((m) => m.SharedModule),
+    },
+    {
+        path: 'registration',
+        loadChildren: () =>
+            import('./shared/shared.module').then((m) => m.SharedModule),
+    },
+    {
+        path: 'courses',
+        loadChildren: () =>
+            import('./features/courses/courses.module').then(
+                (m) => m.CoursesModule
+            ),
+    },
+    {
+        path: '**',
+        redirectTo: '/courses',
+    },
 ];
