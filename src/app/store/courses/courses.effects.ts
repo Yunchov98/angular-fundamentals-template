154140799f7b4bc96041aecd6e780e6e76c2de02
Yunchov98 <georgiiunchov@gmail.com>
2024-10-05 22:54:24 +0300

    Create courses effects

--- src/app/store/courses/courses.effects.ts
+++ src/app/store/courses/courses.effects.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@angular/core';
+import { catchError, map, mergeMap, of } from 'rxjs';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+
+import { CoursesService } from '@app/services/courses.service';
+import {
+    requestAllCourses,
+    requestAllCoursesFail,
+    requestAllCoursesSuccess,
+} from './courses.actions';
 
 @Injectable()
 export class CoursesEffects {
-    constructor() {}
+    constructor(
+        private actions$: Actions,
+        private coursesService: CoursesService
+    ) {}
 
-    // Add your code here
+    getAll$ = createEffect(
+        () =>
+            this.actions$.pipe(
+                ofType(requestAllCourses),
+                mergeMap(() =>
+                    this.coursesService.getAll().pipe(
+                        map((response) =>
+                            requestAllCoursesSuccess({
+                                courses: response.result,
+                            })
+                        ),
+                        catchError((error) =>
+                            of(requestAllCoursesFail({ error: error.message }))
+                        )
+                    )
+                )
+            ),
+        { dispatch: false }
+    );
 }
