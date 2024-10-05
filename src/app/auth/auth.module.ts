1c463eb3cd4e628365284da2e7df57d0ac9b0278
Yunchov98 <georgiiunchov@gmail.com>
2024-10-05 18:09:57 +0300

    Import auth routing module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SessionStorageService } from "./services/session-storage.service";
-import { AuthService } from "./services/auth.service";
 import { HttpClientModule } from '@angular/common/http';
 
+import { AuthService } from './services/auth.service';
+import { SessionStorageService } from './services/session-storage.service';
+
+import { AuthRoutingModule } from './auth-routing.module';
+
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    HttpClientModule
-  ],
-  providers: [
-    SessionStorageService,
-    AuthService,
-  ]
+    declarations: [],
+    imports: [CommonModule, HttpClientModule, AuthRoutingModule],
+    providers: [SessionStorageService, AuthService],
 })
-export class AuthModule { }
+export class AuthModule {}
