046a3415f321cb3f74ff04bc788ba0a75cee7078
Yunchov98 <georgiiunchov@gmail.com>
2024-10-05 18:13:04 +0300

    Add courses generic interface

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,13 +1,15 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import Course from '@app/core/interfaces/course';
+import { CoursesGeneric } from '@app/core/interfaces/coursesGeneric';
+
+import Author from '@app/core/interfaces/author';
+import CourseForm from '@app/core/interfaces/course-form';
 
 import { CONSTANTS } from '@app/core/environments/constants';
 import { ENDPOINTS } from '@app/core/environments/endpoints';
-import Author from '@app/core/interfaces/author';
-import CourseForm from '@app/core/interfaces/course-form';
 
 @Injectable({
     providedIn: 'root',
@@ -17,33 +19,28 @@ export class CoursesService {
 
     constructor(private http: HttpClient) {}
 
-    getAll(): Observable<{ successful: boolean; result: Course[] }> {
-        return this.http.get<{ successful: boolean; result: Course[] }>(
+    getAll(): Observable<CoursesGeneric<Course[]>> {
+        return this.http.get<CoursesGeneric<Course[]>>(
             this.apiUrl + ENDPOINTS.courses
         );
     }
 
-    createCourse(
-        course: CourseForm
-    ): Observable<{ successful: boolean; result: Course }> {
-        return this.http.post<{ successful: boolean; result: Course }>(
+    createCourse(course: CourseForm): Observable<CoursesGeneric<Course>> {
+        return this.http.post<CoursesGeneric<Course>>(
             this.apiUrl + ENDPOINTS.addCourse,
             course
         );
     }
 
-    editCourse(
-        id: string,
-        course: Course
-    ): Observable<{ successful: boolean; result: Course }> {
-        return this.http.put<{ successful: boolean; result: Course }>(
+    editCourse(id: string, course: Course): Observable<CoursesGeneric<Course>> {
+        return this.http.put<CoursesGeneric<Course>>(
             this.apiUrl + ENDPOINTS.singleCourse(id),
             course
         );
     }
 
-    getCourse(id: string): Observable<{ successful: boolean; result: Course }> {
-        return this.http.get<{ successful: boolean; result: Course }>(
+    getCourse(id: string): Observable<CoursesGeneric<Course>> {
+        return this.http.get<CoursesGeneric<Course>>(
             this.apiUrl + ENDPOINTS.singleCourse(id)
         );
     }
@@ -54,12 +51,10 @@ export class CoursesService {
         );
     }
 
-    filterCourses(
-        value: string
-    ): Observable<{ successful: boolean; result: Course[] }> {
+    filterCourses(value: string): Observable<CoursesGeneric<Course[]>> {
         const params = new HttpParams().set('title', value);
 
-        return this.http.get<{ successful: boolean; result: Course[] }>(
+        return this.http.get<CoursesGeneric<Course[]>>(
             this.apiUrl + ENDPOINTS.filteredCourses,
             {
                 params,
@@ -67,25 +62,21 @@ export class CoursesService {
         );
     }
 
-    getAllAuthors(): Observable<{ successful: boolean; result: Author[] }> {
-        return this.http.get<{ successful: boolean; result: Author[] }>(
+    getAllAuthors(): Observable<CoursesGeneric<Author[]>> {
+        return this.http.get<CoursesGeneric<Author[]>>(
             this.apiUrl + ENDPOINTS.authors
         );
     }
 
-    createAuthor(
-        name: string
-    ): Observable<{ successful: boolean; result: Author }> {
-        return this.http.post<{ successful: boolean; result: Author }>(
+    createAuthor(name: string): Observable<CoursesGeneric<Author>> {
+        return this.http.post<CoursesGeneric<Author>>(
             this.apiUrl + ENDPOINTS.createAuthor,
             { name }
         );
     }
 
-    getAuthorById(
-        id: string
-    ): Observable<{ successful: boolean; result: Author }> {
-        return this.http.get<{ successful: boolean; result: Author }>(
+    getAuthorById(id: string): Observable<CoursesGeneric<Author>> {
+        return this.http.get<CoursesGeneric<Author>>(
             this.apiUrl + ENDPOINTS.singleAuthor(id)
         );
     }
