ef2ff350101eea6e98a970addf2cbfa2a19f0d2d
Yunchov98 <georgiiunchov@gmail.com>
2024-10-05 18:12:35 +0300

    Import courses routring module

--- src/app/features/courses/courses.module.ts
+++ src/app/features/courses/courses.module.ts
@@ -1,35 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '@app/shared/shared.module';
+import { CoursesRoutingModule } from './courses-routing.module';
 
 import { CourseListComponent } from './course-list/course-list.component';
-import { RouterModule, Routes } from '@angular/router';
-import {
-    CourseCardComponent,
-    CourseFormComponent,
-} from '@app/shared/components';
-import { CoursesComponent } from './courses.component';
 
-import { AdminGuard } from '@app/user/guards/admin.guard';
 
 const components = [CourseListComponent];
 
-const routes: Routes = [
-    { path: 'courses', component: CoursesComponent },
-    { path: 'add', canActivate: [AdminGuard], component: CourseFormComponent },
-    {
-        path: 'edit/:id',
-        canActivate: [AdminGuard],
-        component: CourseFormComponent,
-    },
-    {path: 'courses/filter', component: CoursesComponent},
-    { path: 'courses/:id', component: CourseCardComponent },
-];
-
 @NgModule({
     declarations: [components],
-    imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
+    imports: [CommonModule, SharedModule, CoursesRoutingModule],
     exports: [components, RouterModule],
 })
 export class CoursesModule {}
