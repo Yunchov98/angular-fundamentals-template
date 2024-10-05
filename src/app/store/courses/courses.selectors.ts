5afc8d255203b49941b3623cdfef9e60474386a2
Yunchov98 <georgiiunchov@gmail.com>
2024-10-05 22:53:45 +0300

    Create courses selector

--- src/app/store/courses/courses.selectors.ts
+++ src/app/store/courses/courses.selectors.ts
@@ -1,1 +1,42 @@
-// Add your code here
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
+import { CoursesState } from '@app/core/interfaces/coursesState';
+import { coursesFeatureKey } from '@app/core/environments/constants';
+
+export const selectCoursesState =
+    createFeatureSelector<CoursesState>(coursesFeatureKey);
+
+export const isAllCoursesLoadingSelector = createSelector(
+    selectCoursesState,
+    (state: CoursesState) => state.isAllCoursesLoading
+);
+
+export const isSearchingStateSelector = createSelector(
+    selectCoursesState,
+    (state: CoursesState) => state.isSearchState
+);
+
+export const isSingleCourseLoadingSelector = createSelector(
+    selectCoursesState,
+    (state: CoursesState) => state.isSingleCourseLoading
+);
+
+export const getCourses = createSelector(
+    selectCoursesState,
+    (state: CoursesState) => state.allCourses
+);
+
+export const getAllCourses = createSelector(
+    selectCoursesState,
+    (state: CoursesState) => state.allCourses
+);
+
+export const getCourse = createSelector(
+    selectCoursesState,
+    (state: CoursesState) => state.course
+);
+
+export const getErrorMessage = createSelector(
+    selectCoursesState,
+    (state: CoursesState) => state.errorMessage
+);
