a4c65a16a3bb5a045766ecd96158e4b7b8ad5408
Yunchov98 <georgiiunchov@gmail.com>
2024-10-09 20:44:37 +0300

    Add course info component on id route

--- src/app/features/courses/courses-routing.module.ts
+++ src/app/features/courses/courses-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { CoursesComponent } from './courses.component';
+import { AdminGuard } from '@app/user/guards/admin.guard';
 import { AuthorizedGuard } from '@app/auth/guards/authorized.guard';
 
-import { AdminGuard } from '@app/user/guards/admin.guard';
-import {
-    CourseCardComponent,
-    CourseFormComponent,
-} from '@app/shared/components';
+import { CoursesComponent } from './courses.component';
+import { CourseFormComponent } from '@app/shared/components';
+import { CourseInfoComponent } from '../course-info/course-info.component';
 
 const routes: Routes = [
     {
@@ -30,7 +28,7 @@ const routes: Routes = [
     {
         path: ':id',
         canActivate: [AuthorizedGuard],
-        component: CourseCardComponent,
+        component: CourseInfoComponent,
     },
 ];
 
